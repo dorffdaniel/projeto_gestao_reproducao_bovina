8071de8e8717d45c2fb298abe3a2776eeaaaa9b6
Dorffdaniel <danielcontabill@hotmail.com>
2026-07-26 01:13:53 +0100

    feat: adicionado layout edicao evento D0

--- src/app/pages/private/protocolos/gerenciar-protocolos/gerenciar-protocolos.ts
+++ src/app/pages/private/protocolos/gerenciar-protocolos/gerenciar-protocolos.ts
@@ -16,7 +16,7 @@ export class GerenciarProtocolos implements OnInit {
   constructor(
     private rota: ActivatedRoute,
     private route: Router,
-    private serv: GerenciarProtocoloService, 
+    private serv: GerenciarProtocoloService,
     private perfiServ: PerfilService
   ) { }
 
@@ -29,14 +29,21 @@ export class GerenciarProtocolos implements OnInit {
     aberto: false
   })
   existeEventoRegistrado = signal({
-    tipo: '', 
+    tipo: '',
     existe: false
   })
 
   evento_protocolo = signal(this.colunasProtocolo());
   evento_d0 = signal(this.colunasEventoD0())
   isLoading = signal(false);
-  nomeResponsavel = signal<string | null>(null); 
+  nomeResponsavel = signal<string | null>(null);
+  estadoD0 = signal('Disponivel');
+  estadoD7 = signal('Bloqueado');
+  estadoIA = signal('Bloqueado');
+  estadoDG = signal('Bloqueado');
+  dadosEventoD0Registrado = signal<any | null>(null);
+  desabilitarBtnD0 = signal(true);
+  esconderBtnEditar = signal(true);
 
   private colunasProtocolo() {
     return {
@@ -57,11 +64,11 @@ export class GerenciarProtocolos implements OnInit {
   }
 
   ngOnInit(): void {
-    this.mostrarDadosperfilResponsavel(); 
+    this.mostrarDadosperfilResponsavel();
     this.getIdProtocolo();
     this.mostrarDadosProtocolo();
     this.mostrarEventosRegistrados();
-    this.mostrarEventoD0(); 
+    this.mostrarEventoD0();
   }
 
   voltarPaginaAnterior() {
@@ -73,11 +80,11 @@ export class GerenciarProtocolos implements OnInit {
   }
 
   async mostrarDadosperfilResponsavel() {
-    
+
     try {
-      const data = await this.perfiServ.getPerfil();      
-      const primNome = data.nome.split(' ')[0]; 
-      this.nomeResponsavel.set(primNome); 
+      const data = await this.perfiServ.getPerfil();
+      const primNome = data.nome.split(' ')[0];
+      this.nomeResponsavel.set(primNome);
 
     } catch (error) {
       console.log(error)
@@ -106,16 +113,13 @@ export class GerenciarProtocolos implements OnInit {
       console.log("Dados protocolo aqui: ", data)
 
     } catch (error) {
-      console.log(error); 
+      console.log(error);
     } finally {
       this.isLoading.set(false);
     }
 
   }
 
-  editarProt() {
-    alert("em andamento")
-  }
 
   abrirFormCadastrarEventoProtocolo(tipo: 'D0' | 'D7' | 'D9') {
 
@@ -177,33 +181,64 @@ export class GerenciarProtocolos implements OnInit {
     let progresso = 0;
 
     if (d0) progresso += 25;
-    
+
     if (d7) progresso += 25
-  
+
     if (ia) progresso += 25
-  
+
     if (dg) progresso += 25
 
     this.barraProgresso.set(progresso);
   }
 
   async mostrarEventoD0() {
-  
+
     try {
       const data = await this.serv.obterEventoD0Registrado(this.idProtocolo)
 
-      console.log("d0 registrado ", data); 
-
-      this.existeEventoRegistrado.set({
-        tipo: 'D0', 
-        existe: true
-      })
+      console.log("d0 registrado ", data);
+      this.estadoD0.set('Concluido');
+      this.estadoD7.set('Disponivel')
+      this.dadosEventoD0Registrado.set(data);
 
     } catch (error) {
       console.log(error)
     }
-  
+
   }
+
+  getClasseCard(estado: string) {
+
+    switch (estado) {
+      case 'Concluido':
+        return 'card-sucesso';
+      case 'Disponivel':
+        return 'card-disponivel';
+
+      default:
+        return 'card-bloqueado';
+    }
+
+  }
+
+  editarEvento(tipo: string) {
+
+    if (tipo == 'D0') {
+      this.desabilitarBtnD0.set(false);
+      this.esconderBtnEditar.set(false);
+    }
+
+  }
+
+  cancelarEdit(tipo: string) {
+
+    if (tipo == 'D0') {
+      this.desabilitarBtnD0.set(true);
+      this.esconderBtnEditar.set(true);
+    }
+
+  }
+
 
 
   limparCampos(tipo: 'D0' | 'D7') {
